120a71e546624201aa1782feead31ee9a7c53ea8
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2024-09-26 15:00:45 +0300

    updated namespace

--- KH.Services/Features/PdfService.cs
+++ KH.Services/Features/PdfService.cs
@@ -2,65 +2,66 @@ using PuppeteerSharp.Media;
 using PuppeteerSharp;
 using DinkToPdf;
 using DinkToPdf.Contracts;
-
-public class PdfService : IPdfService
+namespace KH.Services.Features
 {
-  private readonly IUserService _userService;
-  private readonly IConverter _converter;
-
-  public PdfService(IUserService userService, IConverter converter)
+  public class PdfService : IPdfService
   {
-    _userService = userService;
-    _converter = converter;
-    //_pdfConverter = pdfConverter;
-  }
+    private readonly IUserService _userService;
+    private readonly IConverter _converter;
 
-  public async Task<byte[]> ExportUserDetailsPdfAsync(UserFilterRequest param)
-  {
-    var response = await _userService.GetAsync(param.Id.Value);
-    var entityFromDB = response.Data;
-
-    string filePathForTemplateBody = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "PDF", "CustomerPdfTemplateBody.html");
-    string htmlContentForBody = ReplaceUserPlaceholders(entityFromDB, filePathForTemplateBody);
-
-    string filePathForTemplateLayout = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "PDF", "CustomerPdfLayout.html");
-    string htmlFileContentForLayout = await File.ReadAllTextAsync(filePathForTemplateLayout);
-
-    htmlFileContentForLayout = htmlFileContentForLayout.Replace("{Htmlbody}", htmlContentForBody);
-
-
-    // Convert the HTML content to PDF using DinkToPdf
-    var pdfDocument = new HtmlToPdfDocument()
+    public PdfService(IUserService userService, IConverter converter)
     {
-      GlobalSettings = {
+      _userService = userService;
+      _converter = converter;
+      //_pdfConverter = pdfConverter;
+    }
+
+    public async Task<byte[]> ExportUserDetailsPdfAsync(UserFilterRequest param)
+    {
+      var response = await _userService.GetAsync(param.Id.Value);
+      var entityFromDB = response.Data;
+
+      string filePathForTemplateBody = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "PDF", "CustomerPdfTemplateBody.html");
+      string htmlContentForBody = ReplaceUserPlaceholders(entityFromDB, filePathForTemplateBody);
+
+      string filePathForTemplateLayout = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "PDF", "CustomerPdfLayout.html");
+      string htmlFileContentForLayout = await File.ReadAllTextAsync(filePathForTemplateLayout);
+
+      htmlFileContentForLayout = htmlFileContentForLayout.Replace("{Htmlbody}", htmlContentForBody);
+
+
+      // Convert the HTML content to PDF using DinkToPdf
+      var pdfDocument = new HtmlToPdfDocument()
+      {
+        GlobalSettings = {
                 ColorMode = ColorMode.Color,
                 Orientation = Orientation.Portrait,
                 PaperSize = PaperKind.A3Extra,
                 Out = null, // Out set to null because we want the result as a byte array
             },
-      Objects = {
+        Objects = {
                 new ObjectSettings() {
                     HtmlContent = htmlFileContentForLayout,
                     WebSettings = { DefaultEncoding = "utf-8" },
                 }
             }
-    };
+      };
 
-    // Generate PDF as byte array
-    return _converter.Convert(pdfDocument);
-  }
+      // Generate PDF as byte array
+      return _converter.Convert(pdfDocument);
+    }
 
-  public byte[] GeneratePdf(string htmlContent)
-  {
-    var pdfDocument = new HtmlToPdfDocument
+    public byte[] GeneratePdf(string htmlContent)
     {
-      GlobalSettings = {
+      var pdfDocument = new HtmlToPdfDocument
+      {
+        GlobalSettings = {
                 ColorMode = ColorMode.Color,
                 Orientation = Orientation.Portrait,
                 PaperSize = PaperKind.A4,
                 Margins = new MarginSettings { Top = 10 }
             },
-      Objects = {
+        Objects = {
                 new ObjectSettings
                 {
                     PagesCount = true,
@@ -69,22 +70,24 @@ public class PdfService : IPdfService
                     FooterSettings = { FontSize = 9, Right = "Page [page] of [toPage]" }
                 }
             }
-    };
+      };
 
-    return _converter.Convert(pdfDocument);
-  }
-  private string ReplaceUserPlaceholders(UserDetailsResponse user, string filePath)
-  {
-    string htmlFileContent = File.ReadAllText(filePath);
+      return _converter.Convert(pdfDocument);
+    }
+    private string ReplaceUserPlaceholders(UserDetailsResponse user, string filePath)
+    {
+      string htmlFileContent = File.ReadAllText(filePath);
 
-    // Replace placeholders with user details
-    htmlFileContent = htmlFileContent.Replace("{FullName}", user.FullName)
-                                     .Replace("{Email}", user.Email)
-                                     .Replace("{MobileNumber}", user.MobileNumber)
-                                     .Replace("{Username}", user.Username)
-                                     .Replace("{BirthDate}", user.BirthDate?.ToString("dd/MM/yyyy") ?? "N/A")
-                                     .Replace("{OtherDetails}", $"Roles: {string.Join(", ", user.UserRoles.Select(r => r.Role.NameEn))}");
+      // Replace placeholders with user details
+      htmlFileContent = htmlFileContent.Replace("{FullName}", user.FullName)
+                                       .Replace("{Email}", user.Email)
+                                       .Replace("{MobileNumber}", user.MobileNumber)
+                                       .Replace("{Username}", user.Username)
+                                       .Replace("{BirthDate}", user.BirthDate?.ToString("dd/MM/yyyy") ?? "N/A")
+                                       .Replace("{OtherDetails}", $"Roles: {string.Join(", ", user.UserRoles.Select(r => r.Role.NameEn))}");
 
-    return htmlFileContent;
+      return htmlFileContent;
+    }
   }
 }
+  
