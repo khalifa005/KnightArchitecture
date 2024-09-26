7d1bd9d0f0a7b0956d353e11315fc6960ca7156c
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2024-09-26 10:32:46 +0300

    updated the welcome email template

--- KH.WebApi/Controllers/EmailController.cs
+++ KH.WebApi/Controllers/EmailController.cs
@@ -20,22 +20,9 @@ namespace KH.WebApi.Controllers
     [HttpPost("Send")]
     public async Task<IActionResult> SendOrderEmail([FromForm] MailRequest request)
     {
-      var orderModel = new OrderConfirmationModel
-      {
-        UserName = "John Doe",
-        OrderId = "ORDER12345",
-        InvoiceLink = "https://yourcompany.com/invoice/ORDER12345",
-        Products = new List<ProductModel>
-            {
-                new ProductModel { Name = "Product A", Price = 50.00, Quantity = 1 },
-                new ProductModel { Name = "Product B", Price = 25.00, Quantity = 2 }
-            }
-      };
-
-      //await _emailService.SendOrderConfirmationAsync(orderModel);
       await _emailService.SendEmailAsync(request);
 
-      return Ok("Order confirmation email sent!");
+      return Ok("Confirmation Email Sent!");
     }
   }
 }
