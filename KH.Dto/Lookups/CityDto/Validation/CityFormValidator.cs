2291470b7546c27c492fbc9fcc2494da1cf7679e
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2024-09-29 14:05:02 +0300

    updated pdf tempalte styles

--- KH.Dto/Lookups/CityDto/Validation/CityFormValidator.cs
+++ KH.Dto/Lookups/CityDto/Validation/CityFormValidator.cs
@@ -1,28 +1,26 @@
 using FluentValidation;
 using KH.Dto.lookups.CityDto.Form;
 
-namespace KH.Dto.lookups.CityDto.Validation
+namespace KH.Dto.lookups.CityDto.Validation;
+
+public class DepartmentFormValidator : AbstractValidator<CityForm>
 {
-  public class DepartmentFormValidator : AbstractValidator<CityForm>
+  public DepartmentFormValidator()
   {
-    public DepartmentFormValidator()
-    {
-      RuleFor(x => x.NameAr)
-        .NotNull()
-        .NotEmpty()
-        .Length(1, 250);
+    RuleFor(x => x.NameAr)
+      .NotNull()
+      .NotEmpty()
+      .Length(1, 250);
 
-      RuleFor(x => x.NameEn)
-        .NotNull()
-        .NotEmpty()
-        .Length(1, 250);
+    RuleFor(x => x.NameEn)
+      .NotNull()
+      .NotEmpty()
+      .Length(1, 250);
 
-      RuleFor(x => x.Description)
-        .NotNull()
-        .NotEmpty()
-        .Length(1, 250)
-        .WithMessage("please-enter-description");
-    }
+    RuleFor(x => x.Description)
+      .NotNull()
+      .NotEmpty()
+      .Length(1, 250)
+      .WithMessage("please-enter-description");
   }
-
 }
