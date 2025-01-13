2bb8e1e1c5982c1b26fb39cba8d98a8bdb31c07a
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2025-01-13 13:58:19 +0300

    updated ui

--- KH.WebDashboard/src/app/pages/cards-examples/cards-examples.component.ts
+++ KH.WebDashboard/src/app/pages/cards-examples/cards-examples.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { I18nService } from '../../@i18n/services/i18n.service';
 import { LanguageEnum } from '../../@i18n/models/language.enum';
 import { PieChartTotal } from '../../@core/models/client/echart';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cards-examples',
@@ -12,34 +13,44 @@ export class CardsExamplesComponent {
 
   constructor(
     private i18nService: I18nService,
+    private translateService: TranslateService,
   ) {
   
     // this.formRandomNumber = generateRandomNumberBasedOnDate();
     // this.loadExample(this.examples[0]);
     }
   
-statusColumns = [
-  { key: 1, title: this.i18nService.language==  LanguageEnum.En ? 'Assigned' : 'مسجلة' },
-  { key: 2, title: this.i18nService.language==  LanguageEnum.En ? 'In progress' : 'جاري العمل عليها' },
-  { key: 3, title: this.i18nService.language==  LanguageEnum.En ? 'Suspended' : 'موقوفة' },
-];
+    statusColumns = [
+      { key: 1, title: this.translateService.instant('columns.status.assigned') },
+      { key: 2, title: this.translateService.instant('columns.status.inProgress') },
+      { key: 3, title: this.translateService.instant('columns.status.suspended') },
+    ];
+    
+    zonesColumns = [
+      { key: 1, title: this.translateService.instant('columns.zones.riyadh') },
+      { key: 2, title: this.translateService.instant('columns.zones.jeedah') },
+      { key: 3, title: this.translateService.instant('columns.zones.mekka') },
+      { key: 4, title: this.translateService.instant('columns.zones.taif') },
+    ];
+    
 apiDataForStatus: PieChartTotal[] = [
   { Id: 1, Total: 100 },
   { Id: 2, Total: 200 },
   { Id: 3, Total: 250 }
 ];
-
-zonesColumns = [
-  { key: 1, title: this.i18nService.language==  LanguageEnum.En ? 'Riyadh' : 'الرياض' },
-  { key: 2, title: this.i18nService.language==  LanguageEnum.En ? 'Jeedah' : 'جدة' },
-  { key: 3, title: this.i18nService.language==  LanguageEnum.En ? 'Mekka' : 'مكة المكرمة' },
-  { key: 4, title: this.i18nService.language==  LanguageEnum.En ? 'Taif' : 'لطائف' },
-];
-
 apiDataForZones: PieChartTotal[] = [
   { Id: 1, Total: 100 },
   { Id: 2, Total: 200 },
   { Id: 3, Total: 300 },
   { Id: 4, Total: 500 }
 ];
+
+
+  name: string = '';
+
+  submit() {
+    console.log('Submitted name:', this.name);
+    alert(`Hello, ${this.name}!`);
+  }
+
 }
--- KH.WebDashboard/src/app/pages/home/home.component.ts
+++ KH.WebDashboard/src/app/pages/home/home.component.ts
@@ -10,24 +10,11 @@ import { apiDataItems } from '../../@core/fakeApiData/ApiDataItems';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  public configuration: Config;
-  public apiCustomColumns: any[];
-  public apiData: any[];
 
-  paginationParam: any = {
-    limit: 5,
-    offset: 0,
-    count: -1,
-    sortColumnKey: '',
-    sortOrder: ''
-  };
-  
   constructor(private readonly cdr: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {   
-    this.apiData = apiDataItems;
-    this.apiCustomColumns = apiColumns;
   }
 
   ngAfterViewInit(): void {
@@ -35,36 +22,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
 
-  products = [
-    { name: 'Laptop', price: '$800', quantity: 10 },
-    { name: 'Phone', price: '$500', quantity: 20 },
-    { name: 'Tablet', price: '$300', quantity: 15 },
-    // { name: 'Monitor', price: '$200', quantity: 8 },
-    // { name: 'Keyboard', price: '$50', quantity: 25 },
-    // { name: 'Mouse', price: '$20', quantity: 30 },
-    // { name: 'Headphones', price: '$100', quantity: 12 },
-    // { name: 'Speakers', price: '$150', quantity: 18 },
-    // { name: 'Printer', price: '$120', quantity: 7 },
-    // { name: 'Scanner', price: '$130', quantity: 6 },
-    // { name: 'Router', price: '$90', quantity: 15 },
-    // { name: 'Switch', price: '$70', quantity: 10 },
-    // { name: 'Hard Drive', price: '$200', quantity: 14 },
-    // { name: 'SSD', price: '$150', quantity: 10 },
-    // { name: 'RAM', price: '$80', quantity: 12 },
-    // { name: 'Graphics Card', price: '$500', quantity: 5 },
-    // { name: 'Motherboard', price: '$250', quantity: 8 },
-    // { name: 'Processor', price: '$300', quantity: 9 },
-    // { name: 'Power Supply', price: '$100', quantity: 11 },
-    // { name: 'Case', price: '$60', quantity: 20 }
-  ];
-
   htmlContent: string = '<h3> test h3 html</h3>';
-  name: string = '';
 
-  submit() {
-    console.log('Submitted name:', this.name);
-    alert(`Hello, ${this.name}!`);
-  }
 
   bellIconConfig: NbIconConfig = { icon: 'bell-outline', pack: 'eva' };
   
