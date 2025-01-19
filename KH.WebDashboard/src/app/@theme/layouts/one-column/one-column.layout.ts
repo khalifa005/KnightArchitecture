8b41cc458ec074de0550a7d35b83a2a8bde6357a
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2025-01-19 14:37:21 +0300

    updated layout

--- KH.WebDashboard/src/app/@theme/layouts/one-column/one-column.layout.ts
+++ KH.WebDashboard/src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -1,16 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NbMediaBreakpointsService, NbMenuService, NbSidebarService, NbThemeService } from '@nebular/theme';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 @Component({
   selector: 'ngx-one-column-layout',
   styleUrls: ['./one-column.layout.scss'],
   template: `
     <nb-layout>
-      <nb-layout-header fixed>
+      <nb-layout-header 
+      [ngClass]="isExpandedHeader ? headerClassExpanded : headerClassCompacted "
+      class="custom-header-expanded"  fixed>
         <ngx-header></ngx-header>
       </nb-layout-header>
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive start>
+      <div class="logo-section">
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/a86eb78ce22023a335f5b608f9666955c1e4a0fcd44d2982b3eaa8921839e023?placeholderIfAbsent=true&apiKey=a0d33f5c0ebf4aa6a5461f186b30b21f" alt="Company logo" class="company-logo" />
+      <img src="assets/icons/col.svg" alt="Menu toggle" class="menu-toggle" />
+    </div>
+    <div class="nav-divider"></div>
+
+      <!-- <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/660a5cc04a46775515f28b0da5f1a820598f6b2217cfad89aac7364ed3477e5c?placeholderIfAbsent=true&apiKey=a0d33f5c0ebf4aa6a5461f186b30b21f" alt="Menu toggle" class="menu-toggle" /> -->
         <ng-content select="nb-menu"></ng-content>
       </nb-sidebar>
 
@@ -21,17 +30,31 @@ import { Subject, takeUntil } from 'rxjs';
     </nb-layout>
   `,
 })
-export class OneColumnLayoutComponent implements OnInit, OnDestroy{
+export class OneColumnLayoutComponent implements OnInit, OnDestroy {
   $destroy = new Subject<void>()
+  private sidebarToggleSubscription!: Subscription;
+  private sidebarExpandSubscription!: Subscription;
+  private sidebarCollapseSubscription!: Subscription;
 
-  constructor(  
+  headerClassCompacted = "custom-header-compacted";
+  headerClassExpanded = "custom-header-expanded";
+  isExpandedHeader = true;
+
+  constructor(
     private menuService: NbMenuService,
     private sidebarService: NbSidebarService,
-    private breakpointService: NbMediaBreakpointsService)
-  {
+    private breakpointService: NbMediaBreakpointsService) {
 
   }
   ngOnInit(): void {
+    // Listen to sidebar toggle events
+    this.sidebarToggleSubscription = this.sidebarService.onToggle().subscribe({
+      next: (event) => {
+        this.isExpandedHeader = !this.isExpandedHeader;
+        console.log('Sidebar toggled:', event);
+      },
+    });
+
 
     // this.menuService.onItemClick().pipe(takeUntil(this.$destroy)).subscribe(
     //   {
@@ -48,9 +71,9 @@ export class OneColumnLayoutComponent implements OnInit, OnDestroy{
     //     }
     //   }
     // )
-}
+  }
 
-ngOnDestroy(): void {
-    
-}
+  ngOnDestroy(): void {
+
+  }
 }
