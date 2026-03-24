99b48afd62a383a948d96bbdc875bc7c1fc256ac
mmkhalifa <mmkhalifa@nwc.com.sa>
2026-03-24 05:53:17 +0300

    feat: Introduce Access Control and Medication Manager features with internationalization and updated authentication UIs.

--- MyFamilyHealth.Client/src/app/features/access-control/access-control.ts
+++ MyFamilyHealth.Client/src/app/features/access-control/access-control.ts
@@ -1,13 +1,21 @@
+import { Component, signal } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-access-control',
-  imports: [TranslocoModule, ButtonModule, InputTextModule],
+  imports: [TranslocoModule, ButtonModule, InputTextModule, DialogModule, FormsModule],
   templateUrl: './access-control.html',
   styleUrl: './access-control.scss',
 })
-export class AccessControl {}
+export class AccessControl {
+  displayAddModal = signal(false);
+
+  openAddModal() {
+    this.displayAddModal.set(true);
+  }
+}
--- MyFamilyHealth.Client/src/app/features/medication-manager/medication-manager.ts
+++ MyFamilyHealth.Client/src/app/features/medication-manager/medication-manager.ts
@@ -7,17 +7,19 @@ import { ButtonModule } from 'primeng/button';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   standalone: true,
   selector: 'app-medication-manager',
-  imports: [CommonModule, TranslocoModule, ButtonModule, ToggleSwitchModule, DialogModule, InputTextModule, FormsModule],
+  imports: [CommonModule, TranslocoModule, ButtonModule, ToggleSwitchModule, DialogModule, InputTextModule, FormsModule, CheckboxModule],
   templateUrl: './medication-manager.html',
   styleUrl: './medication-manager.scss',
 })
 export class MedicationManager {
   remindersActive = signal(true);
   displayAddModal = signal(false);
+  isBlacklisted = signal(false);
 
   openAddModal() {
     this.displayAddModal.set(true);
